efab90140dc88617f3a7d16633fe664a9cf40a97
mohamadas <20822443+msounkere@users.noreply.github.com>
2021-03-19 06:45:38 +0000

    Update setup-python.sh
    Update virtualenvwrapper

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 apt-install-if-needed python3 python3-pip python-dev python3-dev python-pip virtualenvwrapper libxml2-dev libxslt1-dev
+pip3 install virtualenv virtualenvwrapper 
 source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 
 function mkvirtualenv-if-needed {
     for envname in $@; do
-        $(lsvirtualenv | grep -q "$envname") || mkvirtualenv "$envname" -p /usr/bin/python3.5
+        $(lsvirtualenv | grep -q "$envname") || mkvirtualenv "$envname" -p /usr/bin/python3.6
     done
 }
