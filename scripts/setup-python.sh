f924e487a84ebcb8f8414d7a9a4bd9aea8eb1097
mohamadas <20822443+msounkere@users.noreply.github.com>
2021-03-19 07:07:34 +0000

    Update setup-python.sh

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-install-if-needed python3 python3-pip python-dev python3-dev python-pip virtualenvwrapper libxml2-dev libxslt1-dev
-pip3 install virtualenv virtualenvwrapper 
+pip3 install virtualenv virtualenvwrapper psycopg2 kombu
 source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 
 function mkvirtualenv-if-needed {
